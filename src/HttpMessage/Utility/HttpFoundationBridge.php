59d3203c3d55e812bfd7cf32d0f6341fe7f6810f
Brad Kent <bkfake-github@yahoo.com>
2024-07-12 14:09:06 -0500

    PHP < 7.0 name already in use error

--- src/HttpMessage/Utility/HttpFoundationBridge.php
+++ src/HttpMessage/Utility/HttpFoundationBridge.php
@@ -13,7 +13,7 @@
 namespace bdk\HttpMessage\Utility;
 
 use bdk\HttpMessage\Response as ResponseImplementation;
-use bdk\HttpMessage\ServerRequest;
+use bdk\HttpMessage\ServerRequest as ServerRequestImplementation;
 use bdk\HttpMessage\Stream;
 use bdk\HttpMessage\UploadedFile;
 use bdk\HttpMessage\Uri as UriImplementation;
@@ -35,11 +35,11 @@ class HttpFoundationBridge
      *
      * @param HttpFoundationRequest $request HttpFoundation\Request obj
      *
-     * @return ServerRequest
+     * @return ServerRequestImplementation
      *
      * @psalm-suppress ReservedWord complains about HttpFoundations' : mixed return spec
      */
-    public static function createRequest(HttpFoundationRequest $request): ServerRequest
+    public static function createRequest(HttpFoundationRequest $request): ServerRequestImplementation
     {
         /** @psalm-var string  */
         $query = $request->server->get('QUERY_STRING', '');
@@ -52,7 +52,7 @@ class HttpFoundationBridge
         $bodyContentResource = $request->getContent(true);
         $stream = new Stream($bodyContentResource);
 
-        $psr7request = new ServerRequest($request->getMethod(), $uri, $request->server->all());
+        $psr7request = new ServerRequestImplementation($request->getMethod(), $uri, $request->server->all());
         $psr7request = $psr7request
             ->withBody($stream)
             ->withUploadedFiles(self::getFiles($request->files->all()))
